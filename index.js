de20e67ebcbb35cefe6bf1fedbdebade860ab10e
Johan <131137640+Johan025@users.noreply.github.com>
2023-06-16 21:44:26 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // evenement bouton afficher version
 const fenetreAng=document.querySelector('.fenetreFrancais');
 const button=document.getElementById('ang');
+const form=document.querySelector('form');
 
 button.addEventListener ('click' , () =>{
    fenetreAng.classList.toggle('show');
@@ -9,7 +10,34 @@ button.addEventListener ('click' , () =>{
 //evenement responsive version telephone
 const button_menu=document.querySelector('.logoBurger');
 const head=document.querySelector('.header');
+const tour=document.querySelector('.t');
+const contact=document.querySelector('.c');
+const about=document.querySelector('.a');
+
 
 button_menu.addEventListener('click',() =>{
     head.classList.toggle('hideshow');
 })
+
+about.addEventListener('click',() =>{
+    head.classList.remove('hideshow');
+})
+
+tour.addEventListener('click',() =>{
+    head.classList.remove('hideshow');
+})
+
+contact.addEventListener('click',() =>{
+    head.classList.remove('hideshow');
+})
+
+
+
+document.querySelector('form').addEventListener("submit", (e)=>{
+    e.preventDefault();
+
+    // if ()
+})
+
+
+
